223dff0777fd09adfa6c1eaf82569e02d03f3c9b
user <antonanton@Antons-Laptop.local>
2026-08-13 19:32:00 +0500

    Unify storefront heading typography

--- resources/views/user/invoice.blade.php
+++ resources/views/user/invoice.blade.php
@@ -14,7 +14,7 @@
 @php
     // Prefer the minified build (php artisan assets:build) when it is present;
     // fall back to the hand-edited source so local editing keeps working.
-    $__cssVer = '4.15.1';
+    $__cssVer = '4.15.2';
     $__cssFile = file_exists(public_path('assets/css/style.build.css'))
         ? 'assets/css/style.build.css'
         : 'assets/css/style.min.css';
